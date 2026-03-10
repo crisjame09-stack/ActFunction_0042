c4493f79f497fc55b21c5c5d013b080536f393a9
crisjame09-stack <crisjame09@gmail.com>
2026-03-10 10:05:40 +0630

    Add files via upload

---
+++ function_no_perameter.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int length, width;
+
+void input()
+{
+    cout << "Enter Length : ";
+    cin >> length;
+    cout << "Enter Width : ";
+    cin >> width;
+}
+
+int rectangleArea()
+{
+    return length * width;
+}
+
+void output()
+{
+    cout << "Result : " << rectangleArea();
+}
+
+int main()
+{
+    input();
+    output();
+}
---
+++ function_with_perameter.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int length, width;
+
+void input()
+{
+    cout << "Enter Length : ";
+    cin >> length;
+    cout << "Enter Width : ";
+    cin >> width;
+}
+
+int rectangleArea(int a, int b)
+{
+    return a * b;
+}
+
+int rectanglePerimeter(int a, int b)
+{
+    return 2 * (a + b);
+}
+
+void output()
+{
+    cout << "Area : " << rectangleArea(length, width) << endl;
+}
+
+int main()
+{
+    input();
+    output();
+    cout << "Perimeter : " << rectanglePerimeter(length, width);
+}
